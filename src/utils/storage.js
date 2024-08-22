16f12f507985583fc94f0894249b4138874da60f
Stanley Ifenna Obimkpu <obimkpustanley@gmail.com>
2024-08-22 12:05:12 +0100

    changed to session storage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,10 +1,21 @@
 // eslint-disable-next-line import/no-anonymous-default-export
+// export default {
+//   add: (key, item) => localStorage.setItem(key, item),
+
+//   fetch: (key) => localStorage.getItem(key),
+
+//   remove: (key) => localStorage.removeItem(key),
+
+//   clear: () => localStorage.clear(),
+// };
+
 export default {
-  add: (key, item) => localStorage.setItem(key, item),
+  add: (key, item) => sessionStorage.setItem(key, item),
 
-  fetch: (key) => localStorage.getItem(key),
+  fetch: (key) => sessionStorage.getItem(key),
 
-  remove: (key) => localStorage.removeItem(key),
+  remove: (key) => sessionStorage.removeItem(key),
 
-  clear: () => localStorage.clear(),
+  clear: () => sessionStorage.clear(),
 };
+
